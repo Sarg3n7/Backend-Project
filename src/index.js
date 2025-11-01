1edda99c03a90d183821e22b2bc411c310c7090a
Sarg3n7 <shashwatshivam04@gmail.com>
2025-11-01 23:59:37 +0530

    established database connection

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from 'dotenv'
+import connectDB from "./db/DB.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The  below code in an approach to connect database with backend
+/*
+import express from "express";
+const app = express();
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error
+    }
+})()
+*/
