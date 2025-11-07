4c16eb38ea4804609b9cc9285b35bd1aa910f99f
Sarg3n7 <shashwatshivam04@gmail.com>
2025-11-08 01:53:22 +0530

    implemented file handling using multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,16 +13,20 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
-    } catch (error) {
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("File is uploaded on cloudinary",
+            response.url);
+        return response
         
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
+        return null;
     }
 }
 
 
 
-
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-    {public_id: "olympic_flag"},
-    function(error, result) {console.log(result);
-    }
-)
+export {uploadOnCloudinary};
