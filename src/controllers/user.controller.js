09f3cbd08ee25c4b942cd45e4304f13a56722b0e
Sarg3n7 <shashwatshivam04@gmail.com>
2025-11-15 20:20:02 +0530

    Add user channel profile retrieval: implement logic to fetch user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -390,6 +390,84 @@ const updateUserCoverImage = asyncHandler(async(req, res) =>{
     .json(new ApiResponse(200, user, "Cover image updated successfully"))
 })
 
+
+const getUserChannelProfile = asyncHandler(async(req, res) =>{
+    const{username} = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400,"Usernam is required")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField:"channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size: "$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond: {
+                        if:{$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+                createdAt: 1
+            }
+        }
+    ])
+    console.log(channel);
+    
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "User channel profile fetched successfully"
+        )
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -400,5 +478,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    deleteUserAvatar
+    deleteUserAvatar,
+    getUserChannelProfile
 }
